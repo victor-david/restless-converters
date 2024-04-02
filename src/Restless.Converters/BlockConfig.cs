f3124dfe205cabdb187c6c0f0b83936b50dfad7a
Victor D. Sandiego <codepuppy@balticsoap.com>
2024-04-02 10:39:56 -0600

    Split config into two classes

--- src/Restless.Converters/BlockConfig.cs
+++ src/Restless.Converters/BlockConfig.cs
@@ -7,35 +7,15 @@ namespace Restless.Converters
     /// <summary>
     /// Represents the configuration for a block element
     /// </summary>
-    public class BlockConfig
+    public class BlockConfig : TextElementConfig
     {
         #region Private
-        private double fontSize;
         private double spacing;
         #endregion
 
         /************************************************************************/
 
         #region Public properties
-        /// <summary>
-        /// Gets the block config id
-        /// </summary>
-        public string Id { get; }
-
-        /// <summary>
-        /// Gets or sets the font size.
-        /// </summary>
-        public double FontSize
-        {
-            get => fontSize;
-            set => fontSize = Math.Clamp(value, 8.0, 100.0);
-        }
-
-        /// <summary>
-        /// Gets or sets the font weight
-        /// </summary>
-        public FontWeight FontWeight { get; set; }
-
         /// <summary>
         /// Gets or sets the text alignment
         /// </summary>
@@ -48,16 +28,6 @@ namespace Restless.Converters
         /// This property only affects an image
         /// </remarks>
         public HorizontalAlignment HorizontalAlignment { get; set; }
-
-        /// <summary>
-        /// Gets or sets the background brush
-        /// </summary>
-        public Brush Background { get; set; }
-
-        /// <summary>
-        /// Gets or sets the foreground brush
-        /// </summary>
-        public Brush Foreground { get; set; }
 
         /// <summary>
         /// Gets or sets the border brush
@@ -115,12 +85,8 @@ namespace Restless.Converters
         /// <param name="id">The block id</param>
         /// <param name="fontSize">The font size</param>
         /// <param name="fontWeight">The font weight</param>
-        public BlockConfig(string id, double fontSize, FontWeight fontWeight)
+        public BlockConfig(string id, double fontSize, FontWeight fontWeight) : base(id, fontSize, fontWeight)
         {
-            ArgumentException.ThrowIfNullOrEmpty(id, nameof(id));
-            Id = id;
-            FontSize = fontSize;
-            FontWeight = fontWeight;
             TextAlignment = TextAlignment.Left;
             Spacing = double.NaN;
         }
---
+++ src/Restless.Converters/TextElementConfig.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Restless.Converters
+{
+    /// <summary>
+    /// Represents configuration that may be applied to a text element.
+    /// </summary>
+    public class TextElementConfig
+    {
+        #region Private
+        private double fontSize;
+        #endregion
+
+        /************************************************************************/
+
+        #region Properties
+        /// <summary>
+        /// Gets the config id
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary>
+        /// Gets or sets the background brush
+        /// </summary>
+        public Brush Background { get; set; }
+
+        /// <summary>
+        /// Gets or sets the foreground brush
+        /// </summary>
+        public Brush Foreground { get; set; }
+
+        /// <summary>
+        /// Gets or sets the font size.
+        /// </summary>
+        public double FontSize
+        {
+            get => fontSize;
+            set => fontSize = Math.Max(value, 0);
+        }
+
+        /// <summary>
+        /// Gets or sets the font weight
+        /// </summary>
+        public FontWeight FontWeight { get; set; }
+        #endregion
+
+        /************************************************************************/
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextElementConfig"/> class
+        /// </summary>
+        /// <param name="id">The config id</param>
+        public TextElementConfig(string id) : this(id, 12.0)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BlockConfig"/> class
+        /// </summary>
+        /// <param name="id">The config id</param>
+        /// <param name="fontSize">The font size</param>
+        public TextElementConfig(string id, double fontSize) : this(id, fontSize, FontWeights.Normal)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextElementConfig"/> class
+        /// </summary>
+        /// <param name="id">The config id</param>
+        /// <param name="fontSize">The font size</param>
+        /// <param name="fontWeight">The font weight</param>
+        public TextElementConfig(string id, double fontSize, FontWeight fontWeight)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(id, nameof(id));
+            Id = id;
+            FontSize = fontSize;
+            FontWeight = fontWeight;
+        }
+        #endregion
+    }
+}
